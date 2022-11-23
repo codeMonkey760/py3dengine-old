56666f9f7754e8d9fcd34e2191808b2cbbb8c76d
Sean Paget <codeMonkey760@gmail.com>
2022-11-23 14:42:13 -0800

    Add GLAD and GLFW dependencies; got a basic window running; got basic timing working

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Py3DEngine
-
+## Py3DEngine
 3D graphics engine scriptable with python
+
+### Compiling From Source
+1) Install a C/C++ tool chain
+2) Install CMake
+3) Install Python (Ensure that python is discoverable by CMake)
+4) Install GLAD2 into python
+5) Install GLFW3
+6) Configure with CMake
+7) Execute build script (Likely a Makefile)
--- src/main.c
+++ src/main.c
@@ -1,6 +1,87 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
 #include <stdio.h>
 
+#define LOG_TRACE "TRACE"
+#define LOG_INFO "INFO"
+#define LOG_ERROR "ERROR"
+
+static void error_callback(int code, const char* description) {
+    fprintf(stderr, "[%s]: %s 0x%x %s\n", LOG_ERROR, "GLFW error code", code, description);
+}
+
+static void level_log(const char *level, const char *message) {
+    printf("[%s]: %s\n", level, message);
+}
+
+static void info_log(const char *message) {
+    level_log(LOG_INFO, message);
+}
+
+static void trace_log(const char *message) {
+    level_log(LOG_TRACE, message);
+}
+
+static void update(float dt) {
+    static int frame_count = 0;
+    static float since_last_report = 0.0f;
+
+    since_last_report += dt;
+    frame_count += 1;
+
+    if (since_last_report >= 1.0f) {
+        float mpf = (since_last_report / (float) frame_count) * 1000.0f;
+        printf("FPS: %d MPF: %f\n", frame_count, mpf);
+
+        frame_count = 0;
+        while (since_last_report >= 1.0f) {
+            since_last_report -= 1.0f;
+        }
+    }
+}
+
 int main() {
-    printf("Hello, World!\n");
+    glfwSetErrorCallback(error_callback);
+
+    trace_log("Initializing GLFW");
+    if (!glfwInit()) {
+        return 1;
+    }
+    trace_log("GLFW successfully initialized");
+
+    trace_log("Creating GLFW window");
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Py3DEngine", NULL, NULL);
+    if (!window) {
+        return 1;
+    }
+    trace_log("Successfully created a window");
+
+    trace_log("Creating OpenGL context and binding it to new window");
+    glfwMakeContextCurrent(window);
+
+    trace_log("Loading GLAD");
+    int version = gladLoadGL(glfwGetProcAddress);
+    printf("GL %d.%d\n",GLAD_VERSION_MAJOR(version),GLAD_VERSION_MINOR(version));
+
+    glfwSwapInterval(1);
+    glClearColor(0.25f, 0.25f, 0.75f, 1.0f);
+
+    float prev_ts, cur_ts = 0.0f;
+    while(!glfwWindowShouldClose(window)) {
+        prev_ts = cur_ts;
+        cur_ts = (float) glfwGetTime();
+        float dt = cur_ts - prev_ts;
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        update(dt);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
