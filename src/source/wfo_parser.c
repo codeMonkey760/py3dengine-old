dd3de739cf65dae8415124d3a777cb5b5e38bc8e
Sean P <codeMonkey760@gmail.com>
2022-12-04 18:53:34 -0800

    Continue on wfo parser

--- src/source/wfo_parser.c
+++ src/source/wfo_parser.c
@@ -81,7 +81,7 @@ static void resetLineBuffer(char *lineBuffer) {
 }
 
 static char* readStringFromLine(char *curPos, char *dst, int limit) {
-    if (curPos == NULL || dst == NULL || limit < 0) return curPos;
+    if (curPos == NULL || (*curPos) == 0 || dst == NULL || limit < 0) return curPos;
 
     for (int i = 0; i < limit; ++i) {
         (*dst) = (*curPos++);
@@ -93,14 +93,37 @@ static char* readStringFromLine(char *curPos, char *dst, int limit) {
     return curPos;
 }
 
+static char* readFloatFromLine(char *curPos, float *dst) {
+    if (curPos == NULL || (*curPos) == 0 || dst == NULL) return curPos;
+
+    char *newEnd = NULL;
+    (*dst) = strtof(curPos, &newEnd);
+
+    return newEnd;
+}
+
 static char* advancePastSpaces(char *curPos) {
-    if (curPos == NULL) return NULL;
+    if (curPos == NULL || (*curPos) == 0) return curPos;
 
     while ( (*curPos) == ' ' ) {
         curPos++;
     }
 
     return curPos;
+}
+
+static void appendVertexData(struct VertexListNode **vertexDataListPtr, char *type, float *src, int size) {
+    if (vertexDataListPtr == NULL || src == NULL || type == NULL || size > 3) return;
+
+    struct VertexListNode *newNode = NULL;
+    newNode = calloc(1, sizeof(struct VertexListNode));
+    if (newNode == NULL) return;
+
+    for (int i = 0; i < size; ++i) {
+        newNode->data[i] = src[i];
+    }
+    newNode->data_len = size;
+    strcpy_s(newNode->type, 2, type);
 }
 
 void parseWaveFrontFile(FILE *wfo, struct Model **modelPtr) {
@@ -118,13 +141,24 @@ void parseWaveFrontFile(FILE *wfo, struct Model **modelPtr) {
     char *typeBuffer = calloc(3, sizeof(char));
     char *curPos = NULL;
     float *dataBuffer = calloc(3, sizeof(float));
-    int data_used = 0;
 
     while(fgets(lineBuffer, line_buffer_size_in_elements, wfo) != NULL) {
         curPos = lineBuffer;
         curPos = readStringFromLine(curPos, typeBuffer, 2);
         curPos = advancePastSpaces(curPos);
-        curPos = readFloatFromLine(curPos, )
+        curPos = readFloatFromLine(curPos, &(dataBuffer[0]));
+        curPos = advancePastSpaces(curPos);
+        curPos = readFloatFromLine(curPos, &(dataBuffer[1]));
+        curPos = advancePastSpaces(curPos);
+        curPos = readFloatFromLine(curPos, &(dataBuffer[2]));
+
+        if (strncmp(typeBuffer, "v", 1) == 0) {
+            appendVertexData(&posList, typeBuffer, dataBuffer, 3);
+        } else if (strncmp(typeBuffer, "vn", 2) == 0) {
+            appendVertexData(&normalList, typeBuffer, dataBuffer, 3);
+        } else if (strncmp(typeBuffer, "vt", 2) == 0) {
+            appendVertexData(&texCoordList, typeBuffer, dataBuffer, 2);
+        }
     }
 
     free(dataBuffer);
