9268eab6c2e23577ede7a3a6c37b8158c60a548a
Sean Paget <codeMonkey760@gmail.com>
2022-12-26 18:25:41 -0800

    Add BaseComponent header

---
+++ src/headers/Components/base_component.h
@@ -0,0 +1,13 @@
+#ifndef PY3DENGINE_BASE_COMPONENT_H
+#define PY3DENGINE_BASE_COMPONENT_H
+
+#include "camera.h"
+
+struct BaseComponent {
+    unsigned int _type;
+    void (*update)(void *, float);
+    void (*render)(void *, struct Camera *);
+    void (*deleter)(void **);
+};
+
+#endif
