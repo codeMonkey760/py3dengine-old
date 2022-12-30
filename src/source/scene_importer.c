a0c1cf4eb1e5d78632fec6ad5ec79c7bb71fa273
Sean Paget <codeMonkey760@gmail.com>
2022-12-29 23:10:07 -0800

    Add routine for parsing GameObject

--- src/source/scene_importer.c
+++ src/source/scene_importer.c
@@ -100,6 +100,45 @@ static void importShader(struct Shader **shaderPtr) {
     newShader = NULL;
 }
 
+static void parseGameObject(json_object *json, struct GameObject *parent, struct GameObject **rootPtr) {
+    if (json == NULL || rootPtr == NULL || (*rootPtr) != NULL) return;
+
+    json_object *json_name = json_object_object_get(json, "name");
+    if (json_name == NULL || !json_object_is_type(json_name, json_type_string)) {
+        error_log("%s", "[SceneImporter]: Game Object must have a string property called \"name\"");
+        return;
+    }
+
+    json_object *json_transform = json_object_object_get(json, "transform");
+    if (json_transform == NULL || !json_object_is_type(json_transform, json_type_object)) {
+        error_log("%s", "[SceneImporter]: Game Object must have an object property called \"transform\"");
+        return;
+    }
+
+    json_object *json_components_array = json_object_object_get(json, "components");
+    if (json_components_array == NULL || !json_object_is_type(json_components_array, json_type_array)) {
+        error_log("%s", "[SceneImporter]: Game Object must have an array property called \"components\"");
+        return;
+    }
+
+    json_object *json_children_array = json_object_object_get(json, "children");
+    if (json_children_array == NULL || !json_object_is_type(json_children_array, json_type_array)) {
+        error_log("%s", "[SceneImporter]: Game Object must have an array property called \"children\"");
+        return;
+    }
+
+    struct GameObject *newGO = NULL;
+    allocGameObject(&newGO);
+    setGameObjectName(newGO, json_object_get_string(json_name));
+
+    if (parent != NULL) {
+        attachChild(parent, newGO);
+    } else {
+        (*rootPtr) = newGO;
+    }
+    newGO = NULL;
+}
+
 void allocSceneImporter(struct SceneImporter **importerPtr) {
     if (importerPtr == NULL || (*importerPtr) != NULL) return;
 
