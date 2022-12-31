7c10c26e11ca4b0704dbf5290717408ba9bad93d
Sean Paget <codeMonkey760@gmail.com>
2022-12-31 14:14:33 -0800

    Add virtual override parse function to RotationComponent

--- src/source/components/rotation_component.c
+++ src/source/components/rotation_component.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 #include "util.h"
+#include "logger.h"
 #include "custom_string.h"
 #include "components/rotation_component.h"
 #include "game_object.h"
@@ -32,6 +33,60 @@ static void update(struct BaseComponent *component, float dt) {
     struct TransformComponent *transform = getGameObjectTransform(getComponentOwner(component));
     if (transform == NULL) return;
     rotateTransform(transform, deltaRot);
+}
+
+static bool parse(struct BaseComponent *component, json_object *json, struct ResourceManager *resource) {
+    if (!isComponentValid(component) || json == NULL || resource == NULL) return false;
+
+    json_object *json_name = json_object_object_get(json, "name");
+    if (json_name == NULL || !json_object_is_type(json_name, json_type_string)) {
+        error_log("%s", "[RotationComponent]: Component must have a string property called \"name\"");
+        return false;
+    }
+
+    json_object *json_axis = json_object_object_get(json, "axis");
+    if (json_axis == NULL || !json_object_is_type(json_axis, json_type_object)) {
+        error_log("%s", "[RotationComponent]: Component must have an object property called \"axis\"");
+        return false;
+    }
+
+    json_object *json_axis_x = json_object_object_get(json_axis, "x");
+    if (json_axis_x == NULL || !json_object_is_type(json_axis_x, json_type_double)) {
+        error_log("%s", "RotationComponent]: Component must have a double property called \"x\" assigned to \"axis\"");
+        return false;
+    }
+
+    json_object *json_axis_y = json_object_object_get(json_axis, "y");
+    if (json_axis_y == NULL || !json_object_is_type(json_axis_x, json_type_double)) {
+        error_log("%s", "RotationComponent]: Component must have a double property called \"y\" assigned to \"axis\"");
+        return false;
+    }
+
+    json_object *json_axis_z = json_object_object_get(json_axis, "z");
+    if (json_axis_z == NULL || !json_object_is_type(json_axis_z, json_type_double)) {
+        error_log("%s", "RotationComponent]: Component must have a double property called \"z\" assigned to \"axis\"");
+        return false;
+    }
+
+    json_object *json_speed = json_object_object_get(json, "speed");
+    if (json_speed == NULL || !json_object_is_type(json_speed, json_type_object)) {
+        error_log("%s", "[RotationComponent]: Component must have a double property called \"speed\"");
+        return false;
+    }
+
+    setComponentName(component, json_object_get_string(json_name));
+
+    struct RotationComponent *rc = (struct RotationComponent *) component;
+
+    float newAxis[3] = {0.0f};
+    newAxis[0] = (float) json_object_get_double(json_axis_x);
+    newAxis[1] = (float) json_object_get_double(json_axis_y);
+    newAxis[2] = (float) json_object_get_double(json_axis_z);
+
+    setRotationComponentAxis(rc, newAxis);
+    setRotationComponentSpeed(rc, (float) json_object_get_double(json_speed));
+
+    return true;
 }
 
 static void delete(struct BaseComponent **componentPtr) {
