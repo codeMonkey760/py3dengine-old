c86f32394089a240211ec4fa281adf8f930f3749
Sean Paget <codeMonkey760@gmail.com>
2022-12-31 13:01:20 -0800

    Add virtual override parse function to ModelRendererComponent

--- src/source/components/model_renderer_component.c
+++ src/source/components/model_renderer_component.c
@@ -1,4 +1,5 @@
 #include "util.h"
+#include "logger.h"
 #include "custom_string.h"
 #include "resources/model.h"
 #include "resources/shader.h"
@@ -7,6 +8,7 @@
 #include "game_object.h"
 #include "components/transform_component.h"
 #include "rendering_context.h"
+#include "resource_manager.h"
 
 #define COMPONENT_TYPE_MODEL_RENDERER 2
 
@@ -48,6 +50,64 @@ static void render(struct BaseComponent *component, struct RenderingContext *ren
     disableShader(mrc->shader);
 }
 
+static bool parse(struct BaseComponent *component, json_object *json, struct ResourceManager *manager) {
+    if (!isComponentValid(component) || json == NULL || manager == NULL) return false;
+
+    json_object *json_name = json_object_object_get(json, "name");
+    if (json_name == NULL || !json_object_is_type(json_name, json_type_string)) {
+        error_log("%s", "[ModelRendererComponent]: Component must have a string property called \"name\"");
+        return false;
+    }
+
+    json_object *json_model_name = json_object_object_get(json, "model");
+    if (json_model_name == NULL || !json_object_is_type(json_model_name, json_type_string)) {
+        error_log("%s", "[ModelRendererComponent]: Component must have a string property called \"model\"");
+        return false;
+    }
+
+    json_object *json_shader_name = json_object_object_get(json, "shader");
+    if (json_shader_name == NULL || !json_object_is_type(json_shader_name, json_type_string)) {
+        error_log("%s", "[ModelRendererComponent]: Component must have a string property called \"shader\"");
+        return false;
+    }
+
+    json_object *json_material_name = json_object_object_get(json, "material");
+    if (json_material_name == NULL || !json_object_is_type(json_material_name, json_type_string)) {
+        error_log("%s", "[ModelRendererComponent]: Component must have a string property called \"material\"");
+        return false;
+    }
+
+    setComponentName(component, json_object_get_string(json_name));
+
+    struct ModelRendererComponent *mrc = (struct ModelRendererComponent *) component;
+
+    const char *newModelName = json_object_get_string(json_model_name);
+    struct Model *newModel = getModelResource(manager, newModelName);
+    if (newModel != NULL) {
+        setModelRendererComponentModel(mrc, newModel);
+    } else {
+        warning_log("[ModelRendererComponent]: Model resource look up failed for \"%s\" while parsing component", newModelName);
+    }
+
+    const char *newShaderName = json_object_get_string(json_shader_name);
+    struct Shader *newShader = getShaderResource(manager, newShaderName);
+    if (newShader != NULL) {
+        setModelRendererComponentShader(mrc, newShader);
+    } else {
+        warning_log("[ModelRendererComponent]: Shader resource look up failed for \"%s\" while parsing component", newShaderName);
+    }
+
+    const char *newMaterialName = json_object_get_string(json_material_name);
+    struct Material *newMaterial = getMaterialResource(manager, newMaterialName);
+    if (newMaterial != NULL) {
+        setModelRendererComponentMaterial(mrc, newMaterial);
+    } else {
+        warning_log("[ModelRendererComponent]: Material resource look up failed for \"%s\" while parsing component", newMaterialName);
+    }
+
+    return true;
+}
+
 static void delete(struct BaseComponent **componentPtr) {
     if (componentPtr == NULL) return;
 
@@ -67,6 +127,7 @@ void allocModelRendererComponent(struct ModelRendererComponent **componentPtr) {
     base->_type = COMPONENT_TYPE_MODEL_RENDERER;
     allocString(&base->_typeName, COMPONENT_TYPE_NAME_MODEL_RENDERER);
     base->render = render;
+    base->parse = parse;
     base->delete = delete;
 
     newComponent->shader = NULL;
