0aaa87136486adf9b51a8a3df45605116a56ca38
Sean Paget <codeMonkey760@gmail.com>
2023-01-14 15:09:19 -0800

    Add test code for Component.get_owner, GameObject.get_name

--- src/resources/TestComponent.py
+++ src/resources/TestComponent.py
@@ -1,5 +1,6 @@
 import py3dengine
 
+
 class TestComponent(py3dengine.Component):
     def __init__(self):
         self.__theta = 0
@@ -9,4 +10,9 @@ class TestComponent(py3dengine.Component):
         if self.__theta > 1.0:
             self.__theta = self.__theta - 1.0
 
-            print("TestComponent::update ... dt was", str(dt))
+            owner = self.get_owner()
+            print('owner is None:', owner is None)
+            print('type(owner).__name__:', type(owner).__name__)
+            print('hasattr(owner, \'get_name\': ', hasattr(owner, 'get_name'))
+            print('callable(owner.get_name):', callable(owner.get_name))
+            print('owner.get_name():', owner.get_name())
