e29972a61f4e212d8d50f07c8f8d57beecb7f1d7
Sean Paget <codeMonkey760@gmail.com>
2023-01-16 13:08:41 -0800

    Modify Rotation Component to test parse method

--- src/resources/RotationComponent.py
+++ src/resources/RotationComponent.py
@@ -3,6 +3,10 @@ import py3dengine
 
 
 class RotationComponent(py3dengine.Component):
+    def __init__(self):
+        self.speed = 0.0
+        self.axis = [0.0, 0.0, 1.0]
+
     def update(self, dt):
         owner = self.get_owner()
         if owner is None:
@@ -14,12 +18,11 @@ class RotationComponent(py3dengine.Component):
             print('My owner doesn\'t have a transform!')
             return
 
-        speed = math.pi / 2.0
-        displacement = speed * dt
+        displacement = self.speed * dt
         fac = math.sin(displacement / 2.0)
-        x = 0.0 * fac
-        y = 0.0 * fac
-        z = 1.0 * fac
+        x = self.axis[0] * fac
+        y = self.axis[1] * fac
+        z = self.axis[2] * fac
         w = math.cos(displacement / 2.0)
 
         length = math.sqrt((x * x) + (y * y) + (z * z) + (w * w))
@@ -29,3 +32,16 @@ class RotationComponent(py3dengine.Component):
         w = w / length
 
         transform.rotate(x, y, z, w)
+
+    def parse(self, values):
+        if 'speed' in values.keys():
+            self.speed = math.radians(float(values['speed']))
+
+        if 'axis' in values.keys():
+            axis = values['axis']
+            if 'x' in axis.keys():
+                self.axis[0] = float(axis['x'])
+            if 'y' in axis.keys():
+                self.axis[1] = float(axis['y'])
+            if 'z' in axis.keys():
+                self.axis[2] = float(axis['z'])
