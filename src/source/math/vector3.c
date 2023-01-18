81518cc4ec9521a0e9cdfa465a64df660ca07526
Sean Paget <codeMonkey760@gmail.com>
2023-01-18 13:47:47 -0800

    Add str, repr and component getters setters to Py3dVector3

--- src/source/math/vector3.c
+++ src/source/math/vector3.c
@@ -9,6 +9,29 @@ struct Py3dVector3 {
 
 static PyObject *py3dVector3Ctor = NULL;
 
+static int setComponent(struct Py3dVector3 *self, PyObject *value, int index) {
+    char names[6] = {'x',0,'y',0,'z',0};
+
+    if (value == NULL) {
+        PyErr_Format(PyExc_TypeError, "Cannot delete the \"%s\" attribute", &names[index * 2]);
+        return -1;
+    }
+
+    Py_INCREF(value);
+    PyObject *flt = PyNumber_Float(value);
+    if (flt == NULL) {
+        PyErr_Format(PyExc_TypeError, "The \"%s\" attribute must be convertible to float", &names[index * 2]);
+        Py_DECREF(value);
+        return -1;
+    }
+
+    self->elements[index] = (float) PyFloat_AsDouble(flt);
+    Py_DECREF(flt);
+    Py_DECREF(value);
+
+    return 0;
+}
+
 static void Py3dVector3_Dealloc(struct Py3dVector3 *self) {
     Py_TYPE(self)->tp_free((PyObject *) self);
 }
@@ -21,6 +44,46 @@ static int Py3dVector3_Init(struct Py3dVector3 *self, PyObject *args, PyObject *
     return 0;
 }
 
+static PyObject *Py3dVector3_GetX(struct Py3dVector3 *self, void *closure) {
+    return PyFloat_FromDouble(self->elements[0]);
+}
+
+static int Py3dVector3_SetX(struct Py3dVector3 *self, PyObject *value, void *closure) {
+    return setComponent(self, value, 0);
+}
+
+static PyObject *Py3dVector3_GetY(struct Py3dVector3 *self, void *closure) {
+    return PyFloat_FromDouble(self->elements[1]);
+}
+
+static int Py3dVector3_SetY(struct Py3dVector3 *self, PyObject *value, void *closure) {
+    return setComponent(self, value, 1);
+}
+
+static PyObject *Py3dVector3_GetZ(struct Py3dVector3 *self, void *closure) {
+    return PyFloat_FromDouble(self->elements[2]);
+}
+
+static int Py3dVector3_SetZ(struct Py3dVector3 *self, PyObject *value, void *closure) {
+    return setComponent(self, value, 2);
+}
+
+static PyObject *Py3dVector3_Repr(struct Py3dVector3 *self) {
+    // TODO: this is hacky, but PyUnicode_FromFormat won't print floats so I blame python 3.10
+    char buffer[65];
+    memset(buffer, 0, 65 * sizeof(char));
+    snprintf(buffer, 64, "Vector3(%.2f, %.2f, %.2f)", self->elements[0], self->elements[1], self->elements[2]);
+
+    PyUnicode_FromString(buffer);
+}
+
+PyGetSetDef Py3dVector3_GettersSetters[] = {
+    {"x", (getter) Py3dVector3_GetX, (setter) Py3dVector3_SetX, "X Component of Vector3", NULL},
+    {"y", (getter) Py3dVector3_GetY, (setter) Py3dVector3_SetY, "Y Component of Vector3", NULL},
+    {"z", (getter) Py3dVector3_GetZ, (setter) Py3dVector3_SetZ, "Z Component of Vector3", NULL},
+    {NULL}
+};
+
 PyMethodDef Py3dVector3_Methods[] = {
     {NULL}
 };
@@ -31,11 +94,14 @@ PyTypeObject Py3dVector3_Type = {
     .tp_doc = "A 3 dimensional vector",
     .tp_basicsize = sizeof(struct Py3dVector3),
     .tp_itemsize = 0,
-    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    .tp_flags = Py_TPFLAGS_DEFAULT,
     .tp_init = (initproc) Py3dVector3_Init,
     .tp_methods = Py3dVector3_Methods,
     .tp_dealloc = (destructor) Py3dVector3_Dealloc,
-    .tp_new = PyType_GenericNew
+    .tp_new = PyType_GenericNew,
+    .tp_repr = (reprfunc) Py3dVector3_Repr,
+    .tp_str = (reprfunc) Py3dVector3_Repr,
+    .tp_getset = Py3dVector3_GettersSetters
 };
 
 bool PyInit_Py3dVector3(PyObject *module) {
