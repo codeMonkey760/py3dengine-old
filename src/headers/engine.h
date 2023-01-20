b4269bb21b9ff7e1fdaafbb2878bbd7d2c19ab23
Sean Paget <codeMonkey760@gmail.com>
2023-01-20 13:33:21 -0800

    Fix bad function declaration

--- src/headers/engine.h
+++ src/headers/engine.h
@@ -1,7 +1,7 @@
 #ifndef PY3DENGINE_ENGINE_H
 #define PY3DENGINE_ENGINE_H
 
-extern void initializeEngine();
+extern void initializeEngine(int argc, char **argv);
 extern void runEngine();
 extern void finalizeEngine();
 
