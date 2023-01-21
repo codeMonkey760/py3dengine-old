81038c592ddffd2cfda582e536c0486fe4366d5c
Sean Paget <codeMonkey760@gmail.com>
2023-01-21 10:21:11 -0800

    Remove error handling from RotationComponent update since the built-in exception handling is more useful

--- src/resources/RotationComponent.py
+++ src/resources/RotationComponent.py
@@ -9,11 +9,7 @@ class RotationComponent(py3dengine.Component):
         self.axis = Vector3(0, 0, 1)
 
     def update(self, dt):
-        try:
-            transform = self.get_owner().get_transform()
-        except AttributeError as err:
-            py3dlogger.error("[RotationComponent]: Could not get owner's transform: " + str(err))
-            return
+        transform = self.get_owner().get_transform()
 
         displacement = Quaternion.FromAxisAndDegrees(self.axis, self.speed * dt)
         transform.rotate(displacement)
