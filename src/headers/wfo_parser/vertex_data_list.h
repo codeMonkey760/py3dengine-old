ca6dd7ca2a57eba3dc3e3886e3a3e6d0d80898d6
Sean Paget <codeMonkey760@gmail.com>
2023-01-26 21:08:45 -0800

    Continue refactoring vertex data list

--- src/headers/wfo_parser/vertex_data_list.h
+++ src/headers/wfo_parser/vertex_data_list.h
@@ -1,24 +1,15 @@
 #ifndef PY3DENGINE_VERTEX_DATA_LIST_H
 #define PY3DENGINE_VERTEX_DATA_LIST_H
 
-#include <stdio.h>
-
-struct Vec2ListNode {
-    struct Vec2ListNode *next;
-    float elements[2];
-};
-
-extern void appendVec2(struct Vec2ListNode **vec2ListNodePtr, float x, float y);
-extern void deleteVec2List(struct Vec2ListNode **vec2ListNodePtr);
-extern void flattenVec2List(struct Vec2ListNode, float *dst, size_t *dstSize);
-
-struct Vec3ListNode {
-    struct Vec3ListNode *next;
+struct VectorListNode {
+    struct VectorListNode *next;
+    int size;
     float elements[3];
 };
 
-extern void appendVec3(struct Vec3ListNode **vec3ListNodePtr, float x, float y, float z);
-extern void deleteVec3List(struct Vec3ListNode **vec3ListNodePtr);
-extern void flattenVec3List(struct Vec3ListNode, float *dst, size_t *dstSize);
+extern void appendVector2(struct VectorListNode **vecListNodePtr, float x, float y);
+extern void appendVector3(struct VectorListNode **vecListNodePtr, float x, float y, float z);
+extern void deleteVectorList(struct VectorListNode **vecListNodePtr);
+extern void flattenVectorList(struct VectorListNode *vecList, float **dstPtr, size_t *dstSizePtr);
 
 #endif
