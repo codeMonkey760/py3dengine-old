a55bb5eb48467509c2c94e0aa5e582027027598c
Sean Paget <codeMonkey760@gmail.com>
2023-02-02 13:56:43 -0800

    Add stringConcatenate function for concating custom string objects

--- src/headers/custom_string.h
+++ src/headers/custom_string.h
@@ -14,6 +14,8 @@ extern void deleteString(struct String **stringPtr);
 extern bool stringEqualsCStr(struct String *s1, const char *s2);
 extern bool stringEquals(struct String *s1, struct String *s2);
 
+extern void stringConcatenate(struct String **stringPtrResult, struct String *s1, struct String *s2);
+
 extern void setChars(struct String *string, const char *chars);
 
 extern unsigned long getLength(struct String *string);
