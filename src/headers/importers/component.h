9c8f3e5ad7bef855f56d1525046bb0b32f258894
Sean Paget <codeMonkey760@gmail.com>
2023-02-05 22:04:52 -0800

    Modify Component importers to use descriptors

--- src/headers/importers/component.h
+++ src/headers/importers/component.h
@@ -2,7 +2,7 @@
 #define PY3DENGINE_IMPORTERS_COMPONENT_H
 
 struct PythonScript;
-extern void importComponent(struct PythonScript **scriptPtr, const char *name);
+extern void importComponent(struct PythonScript **scriptPtr, json_object *componentDesc);
 extern void importBuiltinComponent(struct PythonScript **scriptPtr, const char *name);
 
 #endif
