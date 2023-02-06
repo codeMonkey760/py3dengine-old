cfa7313469e8595d745866efd038c9febfc2d353
Sean Paget <codeMonkey760@gmail.com>
2023-02-05 18:32:57 -0800

    Separate shader importer into its own module

---
+++ src/headers/importers/shader.h
@@ -0,0 +1,7 @@
+#ifndef PY3DENGINE_IMPORTERS_SHADER_H
+#define PY3DENGINE_IMPORTERS_SHADER_H
+
+struct Shader;
+extern void importShader(struct Shader **shaderPtr, const char *fileName);
+
+#endif
--- src/source/scene_importer.c
+++ src/source/scene_importer.c
@@ -12,57 +12,7 @@
 #include "python/python_util.h"
 #include "python/py3denginemodule.h"
 #include "importers/texture.h"
-
-static void importShader(struct Shader **shaderPtr, const char *fileName) {
-    if (shaderPtr == NULL || (*shaderPtr) != NULL || fileName == NULL) return;
-
-    json_object *shader_desc_root = json_object_from_file(fileName);
-
-    json_object *json_type_name = json_object_object_get(shader_desc_root, "type");
-    if (json_type_name == NULL || !json_object_is_type(json_type_name, json_type_string)) {
-        error_log("%s", "[SceneImporter]: Shader description must have an attribute of type \"string\" called \"type\"");
-        return;
-    }
-    if (strcmp(json_object_get_string(json_type_name), "shader") != 0) {
-        error_log("%s", "[SceneImporter]: Shader description type attribute must be set to \"shader\"");
-        return;
-    }
-
-    json_object *json_name = json_object_object_get(shader_desc_root, "name");
-    if (json_name == NULL || !json_object_is_type(json_name, json_type_string)) {
-        error_log("%s", "[SceneImporter]: Shader description must have an attribute of type \"string\" called \"name\"");
-        return;
-    }
-
-    json_object *vertex_shader_file_name = json_object_object_get(shader_desc_root, "vertex_shader_source_file");
-    if (vertex_shader_file_name == NULL || !json_object_is_type(vertex_shader_file_name, json_type_string)) {
-        error_log("%s", "[SceneImporter]: Shader description must have an attribute of type \"string\" called \"vertex_shader_source_file\"");
-        return;
-    }
-
-    json_object *fragment_shader_file_name = json_object_object_get(shader_desc_root, "fragment_shader_source_file");
-    if (fragment_shader_file_name == NULL || !json_object_is_type(fragment_shader_file_name, json_type_string)) {
-        error_log("%s", "[SceneImporter]: Shader description must have an attribute of type \"string\" called \"fragment_shader_source_file\"");
-        return;
-    }
-
-    struct Shader *newShader = NULL;
-    allocShader(&newShader);
-    if (newShader == NULL) return;
-
-    initShaderFromFiles(
-        newShader,
-        json_object_get_string(vertex_shader_file_name),
-        json_object_get_string(fragment_shader_file_name)
-    );
-    setResourceName((struct BaseResource *) newShader, json_object_get_string(json_name));
-
-    (*shaderPtr) = newShader;
-    newShader = NULL;
-
-    json_object_put(shader_desc_root);
-    shader_desc_root = NULL;
-}
+#include "importers/shader.h"
 
 static void importTestScript(struct PythonScript **scriptPtr, const char *name) {
     if (scriptPtr == NULL || (*scriptPtr) != NULL) return;
