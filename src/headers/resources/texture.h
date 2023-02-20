ff79c2dd185eb95345e9f007b021644e849fea00
Sean P <codeMonkey760@gmail.com>
2023-02-20 11:55:40 -0800

    Modify texture initialization so that texture dimensions can be saved during import and later retrieved

--- src/headers/resources/texture.h
+++ src/headers/resources/texture.h
@@ -11,6 +11,8 @@ struct Texture {
     struct BaseResource base;
 
     unsigned int _id;
+    int _width;
+    int _height;
 };
 
 extern bool isResourceTypeTexture(struct BaseResource *resource);
@@ -19,5 +21,9 @@ extern void deleteTexture(struct Texture **texturePtr);
 
 extern void initTexture(struct Texture *texture, const char *fileName);
 extern void setTextureParam(struct Texture *texture, const char *paramName, const char *paramValue);
+
+extern unsigned int getTextureId(struct Texture *texture);
+extern int getTextureWidth(struct Texture *texture);
+extern int getTextureHeight(struct Texture *texture);
 
 #endif
