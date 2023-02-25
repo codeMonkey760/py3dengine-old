cc749695460d2131c7323766f340e57bb30e550e
Sean Paget <codeMonkey760@gmail.com>
2023-02-25 10:50:51 -0800

    Add builtin importer module

---
+++ src/source/importers/builtins.c
@@ -0,0 +1,45 @@
+#include "importers/builtins.h"
+#include "importers/component.h"
+#include "resource_manager.h"
+#include "logger.h"
+#include "resources/model.h"
+
+static void importQuadModel(struct ResourceManager *rm) {
+    if (rm == NULL) return;
+
+    struct Model *quad = NULL;
+    allocModel(&quad);
+    if (quad == NULL) {
+        critical_log("%s", "[BuiltInImporter]: Unable to load Quad Model builtin");
+        return;
+    }
+
+    struct VertexPNT vertices[] = {
+        {-1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f},
+        {-1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f},
+        { 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f},
+
+        { 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f},
+        { 1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f},
+        {-1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f}
+    };
+
+    setModelPNTBuffer(quad, vertices, 6);
+
+    storeResource(rm, (struct BaseResource *) quad);
+    quad = NULL;
+}
+
+void importBuiltInResources(struct ResourceManager *rm) {
+    if (rm == NULL) return;
+
+    struct PythonScript *script = NULL;
+    importBuiltinComponent(&script, "ModelRendererComponent");
+    if (script == NULL) {
+        error_log("%s", "[BuiltInImporter]: Unable to load ModelRendererComponent builtin");
+    }
+    storeResource(rm, (struct BaseResource *) script);
+    script = NULL;
+
+    importQuadModel(rm);
+}
