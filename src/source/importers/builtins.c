847fcc623f1a8f278250aec38f08f20c617bec1e
Sean Paget <codeMonkey760@gmail.com>
2023-02-26 18:07:13 -0800

    Start work on built in sprite shader

--- src/source/importers/builtins.c
+++ src/source/importers/builtins.c
@@ -3,6 +3,30 @@
 #include "resource_manager.h"
 #include "logger.h"
 #include "resources/model.h"
+#include "resources/shader.h"
+
+// TODO: finish builtin sprite shader
+const char *spriteVertexShader =
+"#version 460 core\n"
+"\n"
+"layout(location = 0) in vec3 posL;\n"
+"\n"
+"uniform mat4 gWVPMtx;\n"
+"\n"
+"void main() {\n"
+"   gl_Position = (vec4(posL, 1.0) * gWVPMtx);\n"
+"}\n";
+
+const char *spriteFragShader =
+"#version 460 core\n"
+"\n"
+"uniform vec3 gMixColor;\n"
+"\n"
+"layout(location = 0) out vec4 outputColor;\n"
+"\n"
+"void main() {\n"
+"   "
+;
 
 static void importQuadModel(struct ResourceManager *rm) {
     if (rm == NULL) return;
@@ -29,6 +53,23 @@ static void importQuadModel(struct ResourceManager *rm) {
 
     storeResource(rm, (struct BaseResource *) quad);
     quad = NULL;
+}
+
+static void importSpriteShader(struct ResourceManager *rm) {
+    if (rm == NULL) return;
+
+    struct Shader *shader = NULL;
+    allocShader(&shader);
+    if (shader == NULL) {
+        critical_log("%s", "[BuiltInImporter]: Unable to allocate Sprite Shader");
+        return;
+    }
+
+    setResourceName((struct BaseResource *) shader, "SpriteShader");
+    initShader(shader, spriteVertexShader, spriteFragShader);
+
+    storeResource(rm, (struct BaseResource *) shader);
+    shader = NULL;
 }
 
 void importBuiltInResources(struct ResourceManager *rm) {
