1f06b1a88e1b6a4268b8286a0132cf721fe678bd
Sean Paget <codeMonkey760@gmail.com>
2023-03-25 15:28:23 -0700

    Test ODE by booting it and tearing it down in main

--- src/source/main.c
+++ src/source/main.c
@@ -1,7 +1,12 @@
+#include <ode/ode.h>
+
 #include "logger.h"
 #include "engine.h"
 
 int main(int argc, char **argv) {
+    dInitODE2(0);
+    dCloseODE();
+
     initLogger();
 
     initializeEngine(argc, argv);
