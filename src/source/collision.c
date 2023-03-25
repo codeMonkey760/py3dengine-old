67704cd36e8386e8dc97c7cfa994b0b958e40415
Sean Paget <codeMonkey760@gmail.com>
2023-03-25 16:54:01 -0700

    Create empty collision space and start call dSpaceCollide on it during update cycles

--- src/source/collision.c
+++ src/source/collision.c
@@ -2,14 +2,29 @@
 
 #include "collision.h"
 
+dSpaceID space = NULL;
+
 int initCollisionEngine() {
-    return dInitODE2(0);
+    if (!dInitODE2(0)) return 0;
+
+    space = dSimpleSpaceCreate(0);
+    if (space == NULL) return 0;
+    //destroy geoms registered to this space when its destroyed
+    dSpaceSetCleanup(space, 1);
+
+    return 1;
 }
 
-void handleCollisions() {
+static void nearCallback(void *data, dGeomID o1, dGeomID o2) {
     ;
 }
 
+void handleCollisions() {
+    dSpaceCollide(space, NULL, (dNearCallback *) nearCallback);
+}
+
 void finalizeCollisionEngine() {
+    dSpaceDestroy(space);
+
     dCloseODE();
 }
