85a48373a7ec8d077838c75b686987ee8676291a
Sean Paget <codeMonkey760@gmail.com>
2025-01-19 11:35:37 -0800

    Finish de-allocation routine for LightData struct

--- src/source/lights.c
+++ src/source/lights.c
@@ -2,13 +2,14 @@
 #include "lights.h"
 
 void LightData_Alloc(struct LightData **ptr, unsigned int count) {
-    if (ptr == NULL || (*ptr) != NULL || count == 0) return;
+    if (ptr == NULL || *ptr != NULL || count == 0) return;
 
-   (*ptr) = calloc(count, sizeof(struct LightData));
+   *ptr = calloc(count, sizeof(struct LightData));
 }
 
 void LightData_Dealloc(struct LightData **ptr) {
-    if (ptr == NULL || (*ptr) == NULL) return;
+    if (ptr == NULL || *ptr == NULL) return;
 
-
+    free(*ptr);
+    *ptr = NULL;
 }
